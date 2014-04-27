bb1d4e032db7103aa467d33d0b004e1a1ae67e66
danielno@ucm.es <danielno@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-04-27 17:09:02 +0000

    Corregido un pequeño bug.

--- trunk/vista/directorio.php
+++ trunk/vista/directorio.php
@@ -1,7 +1,7 @@
 <div style="margin-left: 20px;" id="directorio">
 	<p class="fuenteSubtitulo">
 		<?php 
-			session_start();					
+			@session_start();					
 			if (isset($_SESSION['dir'])){
 				echo $_SESSION['dir'];				
 			} else {								
