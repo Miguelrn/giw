b977a74d4ec085ca3b862a3e2fe555055508e3b7
danielno@ucm.es <danielno@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-04-29 12:39:24 +0000

    Agregado top rebajas.

---
+++ vista/tops_discos/top_rebajas.php
@@ -0,0 +1,11 @@
+<?php
+
+
+?>
+<div id="topRebajas">
+	<div class="fuenteTitulo">
+		TOP REBAJAS
+		<!-- <a href="vercesta.php"><img id="carrito" src="vista/images/carrito.png" width="45px"; align="center";></a> -->
+	</div>
+	
+</div>
