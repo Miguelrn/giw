ee4e5be91571cc4593b73301a5ea61fde4f33846
danielno@ucm.es <danielno@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-04-30 12:01:53 +0000

    Corregidos bugs. [Editar edad en modificar perfil]

--- trunk/controlador/cambiarperfil.php
+++ trunk/controlador/cambiarperfil.php
@@ -4,15 +4,17 @@
     
     $nombre = $_POST['nombre'];
     $apellidos = $_POST['apellidos'];
+	$edad = $_POST['edad'];
     $correo = $_POST['correo'];
 	$domicilio = $_POST['domicilio'];
 	$datosBancarios = $_POST['datosBancarios'];
 	
 	$BDD = new Mysql();	
-	$row = $BDD->editarUsuario($correo, $nombre, $apellidos, $domicilio, $datosBancarios);	
+	$row = $BDD->editarUsuarioModificarPerfil($correo, $nombre, $apellidos, $edad, $domicilio, $datosBancarios);	
 	if ($row){
 		$_SESSION['error'] = "Perfil modificado con éxito.";	
 		$_SESSION['nombre'] = $nombre;	
+		$_SESSION['edad'] = $edad;	
 		$_SESSION['correo'] = $correo;	
 		$_SESSION['apellidos'] = $apellidos;	
 		$_SESSION['domicilio'] = $domicilio;	
