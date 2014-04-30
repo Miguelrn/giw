7638a15aa0a92515b545723516dcf1af313d68e4
anguerrero@ucm.es <anguerrero@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-04-30 11:26:46 +0000

    Catalogo....

--- trunk/vista/categorias.php
+++ trunk/vista/categorias.php
@@ -6,30 +6,30 @@
 	</script>	
 	<li class="fuenteTitulo">CATEGORÍAS</li>
 	<li class="fuenteSubtitulo">
-		<a href="#rockandroll" onclick="cargaCatalogo(1)">Rock 'N' Roll</a>
+		<a href="#rockandroll" onclick="cargaCatalogo('Rock%20and%20Roll')">Rock & Roll</a>
 	</li>
 	<li class="fuenteSubtitulo">
-		<a href="#pop" onclick="cargaCatalogo(2)">Pop</a>
+		<a href="#pop" onclick="cargaCatalogo('Pop')">Pop</a>
 	</li>
 	<li class="fuenteSubtitulo">
-		<a href="#electronica" onclick="cargaCatalogo(3)">Electrónica</a>
+		<a href="#electronica" onclick="cargaCatalogo(Electrónica)">Electrónica</a>
 	</li>
 	<li class="fuenteSubtitulo">
-		<a href="#clasica" onclick="cargaCatalogo(4)">Clásica</a>
+		<a href="#clasica" onclick="cargaCatalogo(Clásica)">Clásica</a>
 	</li>
 	<li class="fuenteSubtitulo">
-		<a href="#jazz" onclick="cargaCatalogo(5)">Jazz</a>
+		<a href="#jazz" onclick="cargaCatalogo(Jazz)">Jazz</a>
 	</li>
 	<li class="fuenteSubtitulo">
-		<a href="#rap" onclick="cargaCatalogo(6)">Rap</a>
+		<a href="#rap" onclick="cargaCatalogo(Rap)">Rap</a>
 	</li>
 	<li class="fuenteSubtitulo">
-		<a href="#blues" onclick="cargaCatalogo(7)">Blues</a>
+		<a href="#blues" onclick="cargaCatalogo(Blues)">Blues</a>
 	</li>
 	<li class="fuenteSubtitulo">
-		<a href="#randb" onclick="cargaCatalogo(8)">R&B</a>
+		<a href="#randb" onclick="cargaCatalogo(R&B)">R&B</a>
 	</li>
 	<li class="fuenteSubtitulo">
-		<a href="#folclorica" onclick="cargaCatalogo(9)">Folclórica</a>
+		<a href="#folclorica" onclick="cargaCatalogo(Folclórica)">Folclórica</a>
 	</li>
 </ul>
