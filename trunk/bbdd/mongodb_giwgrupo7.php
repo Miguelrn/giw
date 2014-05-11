0b7ea28f1914112cb2a158048dfe2fc227bd34de
danielno@ucm.es <danielno@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-05-11 07:30:11 +0000

    Idea para importar la bbdd de mongodb en casa y en la uni.

---
+++ trunk/bbdd/mongodb_giwgrupo7.php
@@ -0,0 +1,10 @@
+<?php
+	require_once './controlador/opbasededatosMongoDB.php';
+
+	/**
+	 * Aqui pondría las consultas para insertar los datos en mongo DB, por si no funcionase
+	 * el tema del import y el export.
+	 */
+    
+	
+?>
