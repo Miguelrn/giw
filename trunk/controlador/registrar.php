66623f2707276520c995160396872789bee6e6e1
jmrnavarro@ucm.es <jmrnavarro@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-05-19 12:58:04 +0000

    arreglado registro.php

--- trunk/controlador/registrar.php
+++ trunk/controlador/registrar.php
@@ -2,8 +2,7 @@
 	session_start();
     require_once '../controlador/opbasededatosMongoDB.php';
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    	$BDD = new Mysql();
-	    /*$nombre = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['nombre']))));
+	   /* $nombre = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['nombre']))));
 	    $apellidos = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['apellidos']))));
 		$edad = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['edad']))));
 	    $correo = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['correo']))));        
@@ -11,16 +10,22 @@
 	    $reppass = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['reppass']))));
 		$domicilio = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['domicilio']))));
 		$datosBancarios = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['datosBancarios']))));*/
+		$nombre = $_POST['nombre'];
+	    $apellidos = $_POST['apellidos'];
+		$edad = $_POST['edad'];
+	    $correo = $_POST['correo'];        
+	    $pass = $_POST['pass'];
+	    $reppass = $_POST['reppass'];
+		$domicilio = $_POST['domicilio'];
+		
 		
 		if (strcmp($pass, $reppass) == 0){
 			//Crea una salt al azar
-			$random_salt = hash('sha512', uniqid(mt_rand(1, mt_getrandmax()), true));
 			//Crea una contraseña en salt
-			$password = hash('sha512', $pass.$random_salt);
 			$mongo = new MongoDBConector();		
-			$row = $mongo::insertarUsuario($correo, $password, $nombre,  $apellidos, $edad, $domicilio);
+			$mongo->insertarUsuario($correo, $password, $nombre,  $apellidos, $edad, $domicilio);
 				
-			if ($row){					
+			/*if ($row){	*/				
 				$_SESSION['nombre'] = $nombre;	
 				$_SESSION['correo'] = $correo;	
 				$_SESSION['apellidos'] = $apellidos;	
@@ -33,12 +38,12 @@
 				}
 				header('Location: ../index.php');
 				
-			} else {			
+			/*} else {			
 				$_SESSION['error'] = "Hubo un problema al registrar. Intente nuevamente más tarde.";
 				$_SESSION['logueado'] = false;	
 				header('Location: ../index.php');
 				
-			}
+			}*/
 		} else {		
 			$_SESSION['error'] = "No fue posible realizar el registro. Las contraseñas no coinciden.";
 			$_SESSION['logueado'] = false;			
