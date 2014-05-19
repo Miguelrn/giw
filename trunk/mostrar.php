259e7b0ea722e9876730f9ed10c6bd9c3d97df5e
danielno@ucm.es <danielno@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-05-19 12:36:20 +0000

    Agregado mostrar.php, aún por hacer.

---
+++ trunk/mostrar.php
@@ -0,0 +1,41 @@
+<?php
+	require_once '../controlador/opbasededatosMongoDB.php';
+	
+    $ident = $_GET['ident'];
+    $name = $_GET['name'];
+	$title = $_GET['title'];	
+	
+	// FILTRO
+	
+	
+	
+	
+	// CONSULTA
+	
+	$mongo = new MongoDBConector();		
+	$doc = $mongo->consultaDisco($ident);
+	
+	$nombre = $doc['nombre'];
+	$cantidad = $doc['cantidad'];
+	$categoria = $doc['categoria'];
+	$autor = $doc['autor'];
+	$anno = $doc['anno'];	
+	$foto = $doc['foto'];	
+	$precio = $doc['precio'];
+	
+?>
+<title><? echo $title ?></title>
+<h1><? echo "Bienvenido " . $name ?></h1>
+<img src="vista/images/caratulas/<? echo $foto; ?>" width="300px" height="300px"></img>
+<h2><? echo "Nombre del articulo: " . $name ?></h2>
+<h2><? echo "Cantidad en almacén: " . $cantidad ?></h2>
+<h2><? echo "Autor: " . $autor ?></h2>
+<h2><? echo "Año del disco: " . $anno ?></h2>
+<h2><? echo "Precio: " . $precio ?></h2>
+
+<!--
+	http://localhost/tienda/mostrar.php?ident=""&name=""&title=""
+-->
+
+
+
