bead9064b99ac4938e2cdc0a62ef5753aa44a45f
danielno@ucm.es <danielno@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-05-22 09:10:13 +0000

    Mostrar.php terminado.

--- trunk/controlador/filtros.php
+++ trunk/controlador/filtros.php
@@ -5,44 +5,58 @@
 		
 		public function filtraTitulo($value){
 			// Lalala
+			$value = $this->desinfecta($value);
 			$count = str_word_count($value);
 			if ($count != 1){ return false; }
 			$length = strlen($value);
 			if ($length < 4 || $length > 20){ return false; }
 			$types = is_numeric($value);
 			if ($types){ return false; }	
-			$containsNumber = preg_match('/(.*)[0-9]+(.*)/', $myString);
+			$containsNumber = $this->contieneNumero($value);
 			if ($containsNumber){ return false; }
 			$value = str_replace("\"","",$value);
-			return htmlspecialchars_decode(strip_tags(stripslashes(stripslashes($value))));
+			return $value;
 		}
 		
 		public function filtraIdentificadorDisco($value){
 			// 5379e9ad52cf7da40f00002a
-			/*$count = str_word_count($value);
-			if ($count != 1){ return false; }
+			$value = $this->desinfecta($value);
 			$length = strlen($value);
-			if ($length != 25){ return false; }*/
-			//$containsNumber = preg_match('/(.*)[0-9]+(.*)/', $myString);
-			//if (!$containsNumber){ return false; }
+			if ($length != 26){ return false; }
+			$containsNumber = $this->contieneNumero($value);
+			if (!$containsNumber){ return false; }
 			$value = str_replace("\"","",$value);
-			return htmlspecialchars_decode(strip_tags(stripslashes(stripslashes($value))));
+			return $value;
 		}
 		
 		public function filtraNombreUsuario($value){
 			// Enrique
+			$value = $this->desinfecta($value);
 			$count = str_word_count($value);
 			if ($count != 1){ return false; }
 			$length = strlen($value);
 			if ($length < 4 || $length > 20){ return false; }
 			$types = is_numeric($value);
 			if ($types){ return false; }	
-			$containsNumber = preg_match('/(.*)[0-9]+(.*)/', $myString);
+			$containsNumber = $this->contieneNumero($value);
 			if ($containsNumber){ return false; }
 			$value = str_replace("\"","",$value);
-			return htmlspecialchars_decode(strip_tags(stripslashes(stripslashes($value))));
+			return $value;
 		}
 		
+		public function contieneNumero($value){
+			return strcspn($value, '0123456789') != strlen($value);
+		}
+		
+		public function desinfecta($value){
+			$value = str_replace(";", "", $value);
+			$value = str_replace(".", "", $value);
+			$value = str_replace("-", "", $value);
+			$value = str_replace("_", "", $value);
+			$value = htmlspecialchars_decode(strip_tags(stripslashes($value)));
+			// echo "Desinfectado: " . $value;
+			return $value;			
+		}
 		
 		
 	}
