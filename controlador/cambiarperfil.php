b5a91e34e1ca6c0ce7cb545ec1285ebd2f40019d
danielno@ucm.es <danielno@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-05-25 09:21:02 +0000

    Me sigue sin funcionar el login.

--- controlador/cambiarperfil.php
+++ controlador/cambiarperfil.php
@@ -2,15 +2,26 @@
     require_once '../controlador/opbasededatosMongoDB.php';
 	session_start();
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    	$BDD = new Mysql();
+    	/*$BDD = new Mysql();
 	    $nombre = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['nombre']))));
 	    $apellidos = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['apellidos']))));
 		$edad = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['edad']))));
 	    $correo = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['correo']))));
 		$domicilio = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['domicilio']))));
 		$datosBancarios = $BDD->limpia_sql(htmlspecialchars(trim(strip_tags($_POST['datosBancarios']))));
+		*/
 		
-		$row = $BDD->editarUsuarioModificarPerfil($correo, $nombre, $apellidos, $edad, $domicilio, $datosBancarios);	
+		$nombre = $_POST['nombre'];
+	    $apellidos = $_POST['apellidos'];
+		$edad = $_POST['edad'];
+	    $correo = $_POST['correo'];
+		$domicilio = $_POST['domicilio'];
+		$datosBancarios = $_POST['datosBancarios'];
+		
+		$mongo = new MongoDBConector();
+		$row = $mongo->modificarUsuario($correo, $nombre, $datosBancarios, $apellidos, $edad, $domicilio);
+		
+		//$row = $BDD->editarUsuarioModificarPerfil($correo, $nombre, $apellidos, $edad, $domicilio, $datosBancarios);	
 		if ($row){
 			$_SESSION['error'] = "Perfil modificado con éxito.";	
 			$_SESSION['nombre'] = $nombre;	
