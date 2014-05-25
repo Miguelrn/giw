fecb43cb8f02680ff12d54a545e5977fd6a18dad
danielno@ucm.es <danielno@ucm.es@a29ecc14-9688-470f-aef6-eba27c90da4a>
2014-05-25 16:34:46 +0000

    Tramite casi acabado.

--- controlador/tramitar.php
+++ controlador/tramitar.php
@@ -7,17 +7,19 @@
 	
 	if($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$BDD = new MongoDBConector();
-		$productos = $_SESSION['discos'];//consultar todos los items desde BD? o cookie? desde BD
-		
+		$productos = $_SESSION['discos'];//consultar todos los items desde BD? o cookie? desde BD		
 		$correo = $_SESSION['correo'];
 		$precioDiscos = $_SESSION['precioDiscos'];
 		
-		$id_pedido = $BDD->insertarPedido($correo);//inserta un nuevo pedido en blanco y devuelve un id
+		$BDD->insertarPedido($correo, $precioDiscos, $productos);
+		
+		
+		/*$id_pedido = $BDD->insertarPedido($correo);//inserta un nuevo pedido en blanco y devuelve un id
 		for ($i=0, $len=count($productos); $i<$len; $i++) {
-			$id_pedido = $BDD->insertarArticuloPedido($correo,$id_pedido, $precioDisco);
+			$id_pedido = $BDD->insertarPedido($correo, $id_pedido, $precioDisco);
 			$nombreDisco = $productos[$i][1];
 			$resultado = $BDD->modificarProducto($nombreDisco);
-		}	
+		}*/	
 		$_SESSION['error'] = "Compra realizada correctamente."; 
 		unset($_SESSION['discos']);
 		unset($_SESSION['precioDiscos']);
